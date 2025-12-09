a734e94df61bfe280bddb9ca6f7992280d44baec
Yuan <49439486+Sky-And-Ground@users.noreply.github.com>
2025-12-09 16:24:03 +0800

    Update port_scanner_linux.cpp

--- port_scanner_linux.cpp
+++ port_scanner_linux.cpp
@@ -1,376 +1,289 @@
-/**
- * @author yuan
- * @brief  a port scanner, written in C++11, specailly for linux/bsd platform.
- */
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <fcntl.h>
-#include <poll.h>
-
+// @date   2025-12-08
+// @author yuan
+// @brief  a port scanner written in C++11, only for linux platform, based on epoll mode.
 #include <iostream>
 #include <system_error>
 #include <string>
-#include <bitset>
-#include <thread>
-#include <condition_variable>
-#include <queue>
-#include <functional>
-#include <utility>
 #include <vector>
-#include <chrono>
-#include <cctype>
-#include <cstdlib>
+#include <array>
+#include <cerrno>
 
-#include "lib_config_parser.hpp"
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <sys/epoll.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 // raii wrapper for socket.
 class Socket {
-    int sock = -1;
+    int fd;
 public:
-    Socket() {}
+    Socket() : fd{ -1 } {}
+
+    Socket(int domain, int type, int protocol) {
+        fd = socket(domain, type, protocol);
+        if (fd < 0) {
+            std::error_code ec(errno, std::system_category());
+            throw std::system_error{ ec, "sys call socket failed" };
+        }
+    }
+
+    Socket(int _fd) : fd{ _fd } {}
+
+    Socket(const Socket&) = delete;
+    Socket& operator=(const Socket&) = delete;
+
+    Socket(Socket&& other)
+        : fd{ other.fd }
+    {
+        other.fd = -1;
+    }
+
+    Socket& operator=(Socket&& other) {
+        if (this != &other) {
+            fd = other.fd;
+            other.fd = -1;
+        }
+
+        return *this;
+    }
 
     ~Socket() {
-        if (sock >= 0) {
-            close(sock);
+        if (fd >= 0) {
+            ::close(fd);
         }
     }
 
-    bool open_tcp_mode() {
-        sock = ::socket(AF_INET, SOCK_STREAM, 0);
-
-        if (sock < 0) {
-            return false;
+    void close() {
+        if (fd >= 0) {
+            ::close(fd);
+            fd = -1;
         }
-
-        return true;
     }
 
-    bool connect(const std::string& ip, int port, int timeout_millisec) {
-        struct sockaddr_in addr;
-        addr.sin_family = AF_INET;
-        addr.sin_port = htons(port);
+    void set_nonblock() {
+        int flag = fcntl(fd, F_GETFL, 0);
+        fcntl(fd, F_SETFL, flag | O_NONBLOCK);
+    }
 
-        if (inet_pton(AF_INET, ip.c_str(), &addr.sin_addr) <= 0) {
-            return false;
-        }
-
-        int flags = fcntl(sock, F_GETFL, 0);
-        if (flags == -1) {
-            return false;
-        }
-
-        if (fcntl(sock, F_SETFL, flags | O_NONBLOCK) == -1) {
-            return false;
-        }
-
-        int result = ::connect(sock, (sockaddr*)&addr, sizeof(addr));
-        if (result == 0) {
-            fcntl(sock, F_SETFL, flags);
-            return true;
-        }
-
-        if (errno != EINPROGRESS) {
-            return false;
-        }
-
-        struct pollfd pfd;
-        pfd.fd = sock;
-        pfd.events = POLLOUT;
-        pfd.revents = 0;
-
-        int poll_result = poll(&pfd, 1, timeout_millisec);
-        if (poll_result <= 0) {
-            return false;
-        }
-
-        int error = 0;
-        socklen_t error_len = sizeof(error);
-        if (getsockopt(sock, SOL_SOCKET, SO_ERROR, &error, &error_len) == -1) {
-            return false;
-        }
-
-        fcntl(sock, F_SETFL, flags);
-        return error == 0;
+    int handle() {
+        return fd;
     }
 };
 
-// thread pool.
-using Task = std::function<void()>;
-
-class TaskQueue {
-    std::queue<Task> tasks;
-    std::mutex mut;
-    std::condition_variable cv;
-    bool running = true;
+// raii wrapper for epoll.
+class Epoll {
+    int fd;
 public:
-    void push(Task task) {
-        std::lock_guard<std::mutex> guard{ mut };
-        tasks.emplace(std::move(task));
-
-        cv.notify_one();
+    Epoll() {
+        fd = epoll_create1(0);
+        if (fd < 0) {
+            std::error_code ec(errno, std::system_category());
+            throw std::system_error{ ec, "sys call epoll_create1 failed" };
+        }
     }
 
-    bool pop(Task& task) {
-        std::unique_lock<std::mutex> ulock{ mut };
+    Epoll(const Epoll&) = delete;
+    Epoll& operator=(const Epoll&) = delete;
 
-        while (tasks.empty() && running) {
-            cv.wait(ulock);
-        }
-
-        if (tasks.empty() && !running) {
-            return false;
-        }
-
-        task = std::move(tasks.front());
-        tasks.pop();
-        return true;
+    Epoll(Epoll&& other)
+        : fd{ other.fd }
+    {
+        other.fd = -1;
     }
 
-    void shutdown() {
-        std::lock_guard<std::mutex> guard{ mut };
-        running = false;
-        cv.notify_all();
+    Epoll& operator=(Epoll&& other) {
+        if (this != &other) {
+            fd = other.fd;
+            other.fd = -1;
+        }
+
+        return *this;
+    }
+
+    ~Epoll() {
+        if (fd >= 0) {
+            close(fd);
+        }
+    }
+
+    int handle() {
+        return fd;
+    }
+
+    void add_fd(struct epoll_event* ev, int descriptor) {
+        if (epoll_ctl(fd, EPOLL_CTL_ADD, descriptor, ev) < 0) {
+            std::error_code ec(errno, std::system_category());
+            throw std::system_error{ ec, "sys call epoll_ctl failed on `EPOLL_CTL_ADD`" };
+        }
+    }
+    
+    void del_fd(int descriptor) {
+        if (epoll_ctl(fd, EPOLL_CTL_DEL, descriptor, nullptr) < 0) {
+            std::error_code ec(errno, std::system_category());
+            throw std::system_error{ ec, "sys call epoll_ctl failed on `EPOLL_CTL_DEL`" };
+        }
     }
 };
 
-class ThreadPool {
-    std::vector<std::thread> workers;
-    TaskQueue queue;
-public:
-    ThreadPool(int size) {
-        workers.resize(size);
+// connector, it will do the things.
+template<int N>
+class BatchConnector {
+    struct ConnectRecord {
+        int port;
+        Socket sock;
+        bool opened;
+    };
 
-        for (int i = 0; i < size; ++i) {
-            workers[i] = std::thread{
-                [this]() {
-                    Task task;
-                    while (queue.pop(task)) {
-                        task();
+    std::array<ConnectRecord, N> records;
+    Epoll epoll;
+    int len;
+
+    bool is_connected(int fd) {
+        int error = -1;
+        socklen_t len = sizeof(error);
+
+        int ret = getsockopt(fd, SOL_SOCKET, SO_ERROR, &error, &len);
+        return ret == 0 && error == 0;
+    }
+public:
+    BatchConnector() 
+        : records{}, epoll{}, len{ 0 } 
+    {}
+
+    void submit(const std::string& ip, int port) {
+        // this connector could only hold N elements.
+        if (len == N) {
+            return;
+        }
+
+        auto& record = records[len];
+
+        record.opened = false;
+        record.port = port;
+
+        record.sock = Socket{ AF_INET, SOCK_STREAM, 0 };
+        record.sock.set_nonblock();
+
+        struct sockaddr_in address;
+        address.sin_family = AF_INET;
+        address.sin_port = htons(port);
+        inet_pton(AF_INET, ip.c_str(), &address.sin_addr);
+
+        int ret = connect(record.sock.handle(), (struct sockaddr*)&address, sizeof(address));
+        if (ret < 0) {
+            if (errno == EINPROGRESS) {
+                struct epoll_event ev;
+                ev.events = EPOLLOUT | EPOLLERR | EPOLLHUP;
+                ev.data.ptr = (void*)(records.data() + len);
+
+                epoll.add_fd(&ev, record.sock.handle());
+            }
+            else {
+                record.sock.close();
+            }
+        }
+        else if (ret == 0) {   // hardly to happen.
+            record.opened = true;
+            record.sock.close();
+        }
+
+        ++len;
+    }
+
+    void collect_opened_ports(std::vector<int>& opened_ports, int timeout_millisec) {
+        std::array<struct epoll_event, N> events;
+
+        int nfds = epoll_wait(epoll.handle(), events.data(), events.size(), timeout_millisec);
+        if (nfds > 0) {
+            for (int i = 0; i < nfds; ++i) {
+                ConnectRecord* record = (ConnectRecord*)events[i].data.ptr;
+
+                if (events[i].events & EPOLLOUT) {
+                    if (is_connected(record->sock.handle())) {
+                        record->opened = true;
                     }
                 }
-            };
-        }
-    }
 
-    ~ThreadPool() {
-        shutdown();
-    }
+                if (events[i].events & (EPOLLERR | EPOLLHUP)) {
+                    record->opened = false;
+                }
+            }
 
-    void submit(Task task) {
-        queue.push(task);
-    }
-
-    void shutdown() {
-        queue.shutdown();
-
-        for (std::thread& t : workers) {
-            if (t.joinable()) {
-                t.join();
+            for (int i = 0; i < len; ++i) {
+                if (records[i].opened) {
+                    opened_ports.emplace_back(records[i].port);
+                }
             }
         }
     }
 };
 
-class PortScanner {
-    using PortsTable = std::bitset<65536>;
+template<int N>
+std::vector<int> port_scan(const std::string& ip, const std::vector<int>& ports, int timeout_millisec) {
+    std::vector<int> opened_ports;
 
-    bool port_scan(const std::string& ip, int port, int timeout_millisec) {
-        Socket sock;
-        return sock.open_tcp_mode() && sock.connect(ip, port, timeout_millisec);
-    }
-public:
-    PortsTable scan(const std::string& ip, int port_start, int port_end, int timeout_millisec) {
-        ThreadPool threadPool{ 256 };
-        PortsTable table;
+    int i = 0;
+    int counter = 0;
+    while (i < ports.size()) {
+        BatchConnector<N> connector;
 
-        for (int i = port_start; i <= port_end; ++i) {
-            threadPool.submit([this, &table, &ip, i, timeout_millisec]() {
-                table.set(i, port_scan(ip, i, timeout_millisec));
-                });
+        while (counter < N && i + counter < ports.size()) {
+            connector.submit(ip, ports[i + counter]);
+            ++counter;
         }
 
-        return table;
-    }
-};
+        connector.collect_opened_ports(opened_ports, timeout_millisec);
 
-// some utils.
-int parse_port(const std::string& str) noexcept {
-    int port = 0;
-
-    for (char c : str) {
-        if (isdigit(c)) {
-            port = 10 * port + (c - '0');
-        }
-        else {
-            return -1;
-        }
+        i += counter;
+        counter = 0;
     }
 
-    if (port > 65535) {
-        return -1;
-    }
-
-    return port;
+    return opened_ports;
 }
 
-int parse_positive_integer(const std::string& str) noexcept {
-    int number = 0;
+template<int N>
+std::vector<int> port_scan_range(const std::string& ip, int port_start, int port_end, int timeout_millisec) {
+    std::vector<int> ports;
 
-    for (char c : str) {
-        if (isdigit(c)) {
-            number = 10 * number + (c - '0');
-        }
-        else {
-            return -1;
-        }
+    for (int port = port_start; port <= port_end; ++port) {
+        ports.emplace_back(port);
     }
 
-    return number;
+    return port_scan<N>(ip, ports, timeout_millisec);
 }
 
-// config.
-enum class ConfigExtractError {
-    success,
-
-    not_found_ip,
-    not_found_port_start,
-    not_found_port_end,
-    not_found_timeout_millisec,
-
-    invalid_port_start,
-    invalid_port_end,
-    invalid_timeout_millisec
-};
-
-struct Config {
-    std::string ip;
-    int port_start;
-    int port_end;
-    int timeout_millisec;
-};
-
-const char* config_extract_strerr(ConfigExtractError err) {
-    switch(err) {
-        case ConfigExtractError::success:
-            return "config extract success";
-        case ConfigExtractError::not_found_ip:
-            return "config not found: ip";
-        case ConfigExtractError::not_found_port_start:
-            return "config not found: port_start";
-        case ConfigExtractError::not_found_port_end:
-            return "config not found: port_end";
-        case ConfigExtractError::not_found_timeout_millisec:
-            return "config not found: timeout_millisec";
-        case ConfigExtractError::invalid_port_start:
-            return "config invalid: port_start";
-        case ConfigExtractError::invalid_port_end:
-            return "config invalid: port_end";
-        case ConfigExtractError::invalid_timeout_millisec:
-            return "config invalid: timeout_millisec";
-        default:
-            return "unknown config extract error";
-    }
-}
-
-ConfigExtractError config_extract(const std::map<std::string, std::string>& configMap, Config& config) {
-    const std::string config_ip = "ip";
-    const std::string config_port_start = "port_start";
-    const std::string config_port_end = "port_end";
-    const std::string config_timeout_millisec = "timeout_millisec";
-
-    auto ip_iter = configMap.find(config_ip);
-    if (ip_iter == configMap.cend()) {
-        return ConfigExtractError::not_found_ip;
-    }
-
-    auto port_start_iter = configMap.find(config_port_start);
-    if (port_start_iter == configMap.cend()) {
-        return ConfigExtractError::not_found_port_start;
-    }
-
-    auto port_end_iter = configMap.find(config_port_end);
-    if (port_end_iter == configMap.cend()) {
-        return ConfigExtractError::not_found_port_end;
-    }
-
-    auto timeout_millisec_iter = configMap.find(config_timeout_millisec);
-    if (timeout_millisec_iter == configMap.cend()) {
-        return ConfigExtractError::not_found_timeout_millisec;
-    }
-
-    config.ip = ip_iter->second;
+template<int N>
+std::vector<int> port_scan_commonly_used(const std::string& ip, int timeout_millisec) {
+    std::vector<int> ports;
     
-    int port_start = parse_positive_integer(port_start_iter->second);
-    if (port_start < 0) {
-        return ConfigExtractError::invalid_port_start;
-    }
+    ports.emplace_back(21);    // ftp.
+    ports.emplace_back(22);    // ssh.
+    ports.emplace_back(23);    // telnet.
+    ports.emplace_back(25);    // smtp.
+    ports.emplace_back(53);    // dns.
+    ports.emplace_back(80);    // http.
+    ports.emplace_back(110);   // pop3.
+    ports.emplace_back(443);   // https.
+    ports.emplace_back(1433);  // sql server.
+    ports.emplace_back(3306);  // mysql.
+    ports.emplace_back(5432);  // pgsql.
+    ports.emplace_back(6379);  // redis.
+    ports.emplace_back(8000);
+    ports.emplace_back(8080);
 
-    int port_end = parse_positive_integer(port_end_iter->second);
-    if (port_end < 0) {
-        return ConfigExtractError::invalid_port_end;
-    }
-
-    int timeout_millisec = parse_positive_integer(timeout_millisec_iter->second);
-    if (timeout_millisec < 0) {
-        return ConfigExtractError::invalid_timeout_millisec;
-    }
-
-    config.port_start = (port_start < port_end ? port_start : port_end);
-    config.port_end = (port_start > port_end ? port_start : port_end);
-    config.timeout_millisec = timeout_millisec;
-    return ConfigExtractError::success;
+    return port_scan<N>(ip, ports, timeout_millisec);
 }
 
-// g++ port_scanner.cpp -std=c++11 -l pthread -O2 -s -o port_scanner
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        std::cerr << "usage: " << argv[0] << " <config_file>\n";
-        return 1;
+    auto opened_ports = port_scan_range<256>("192.168.52.167", 0, 65535, 5000);
+
+    for (int port : opened_ports) {
+        std::cout << port << " ";
     }
 
-    try {
-        // get config.
-        config_parser::ConfigParser parser;
-        auto configMap = parser.parse(argv[1]);
-
-        Config config;
-        auto extractRet = config_extract(configMap, config);
-
-        if (extractRet != ConfigExtractError::success) {
-            std::cerr << config_extract_strerr(extractRet) << "\n";
-            return 1;
-        }
-
-        // scan.
-        PortScanner scanner;
-
-        std::cout << "ip: " << config.ip << "\n";
-        std::cout << "ports: " << config.port_start << " to " << config.port_end << "\n";
-        std::cout << "timeout limit: " << config.timeout_millisec << "ms\n";
-        std::cout << "\nscanning...\n";
-
-        auto start = std::chrono::steady_clock::now();
-        auto portsTable = scanner.scan(config.ip, config.port_start, config.port_end, config.timeout_millisec);
-        auto end = std::chrono::steady_clock::now();
-
-        // print result.
-        std::cout << "scan takes " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms\n";
-        std::cout << "\nopened tcp ports: ";
-
-        for (size_t i = 0; i < portsTable.size(); ++i) {
-            if (portsTable.test(i)) {
-                std::cout << i << " ";
-            }
-        }
-
-        std::cout << "\n";
-    }
-    catch(const config_parser::FileNotFoundException& e) {
-        std::cerr << "given config file does not exist\n";
-        return 1;
-    }
-
+    std::cout << "\n";
     return 0;
 }
